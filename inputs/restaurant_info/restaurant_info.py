0685c3509af3f3b34cc625d427242f8fa949cd07
HamzaB505 <hamzaboulaalaop@gmail.com>
2024-10-10 22:32:43 +0200

    feat: update files and new testing of examples with Portixol

--- inputs/restaurant_info/restaurant_info.py
+++ inputs/restaurant_info/restaurant_info.py
@@ -1,27 +1,29 @@
 class RESTAURANT_INFO:
-    name = "Carbone"
-    cuisine = "Italian"
+    name = "Portixol Restaurant"
+    cuisine = "Mediterranean"
 
-    location = {"neighborhood": "Greenwich Village",
-                "city": "New York City",
-                "state": "New York",
-                "country": "United States of America"
-                }
+    location = {
+    "neighborhood": "City Center",
+    "city": "Palma de Mallorca",
+    "state": "Mallorca",
+    "country": "Spain"
+    }
 
     nationalities = [
-                "American",
-                "Americans",
-                "America",
-                "USA",
-                "United States",
-                "United States of America",
-                "Italian",
-                "Italians",
-                "Italy",
-                "French",
-                "Frenchmen",
-                "France"
-                ]
+     "Sweden",
+     "Swedish",
+     "Swede",
+     "Swedes",
+     "Germany",
+     "German",
+     "Germans",
+     "Britain",
+     "British",
+     "Briton",
+     "Britons",
+     "Brit",
+     "Brits"
+    ]
 
     restaurant_context = """
         Portixol Hotel & Restaurant is nestled in the picturesque Portixol harbor, just a short walk from Palma de Mallorca's city center.
@@ -38,3 +40,293 @@ class RESTAURANT_INFO:
         For a more immersive experience, visitors can enjoy the scenic promenade connecting Portixol with Palma or venture to nearby attractions like the Palma Cathedral.
         The laid-back, yet upscale environment of Portixol continues to attract those looking for a balance of relaxation and luxury
         """
+    given_menu = [ ## based on given Portixol Menu
+    {
+        "category": "Starters",
+        "name": "Caviar Oscietra 50gr",
+        "ingredients": "Pure | Natural | Sustainable | Ethical",
+        "price": 155.00
+    },
+    {
+        "category": "Starters",
+        "name": "Amelie No2 Oysters",
+        "ingredients": "",
+        "price": 5.50
+    },
+    {
+        "category": "Starters",
+        "name": "Sashimi PORTIXOL",
+        "ingredients": "",
+        "price": 27.00
+    },
+    {
+        "category": "Starters",
+        "name": "Iberian cured ham",
+        "ingredients": "",
+        "price": 27.00
+    },
+    {
+        "category": "Starters",
+        "name": "Tiger prawns tempura",
+        "ingredients": "",
+        "price": 19.00
+    },
+    {
+        "category": "Starters",
+        "name": "White sea bass ceviche",
+        "ingredients": "King prawns, green papaya, mango",
+        "price": 25.00
+    },
+    {
+        "category": "Starters",
+        "name": "PORTIXOL Toast",
+        "ingredients": "Toast, shrimps, crab",
+        "price": 18.00
+    },
+    {
+        "category": "Starters",
+        "name": "Seasonal tomato tarter",
+        "ingredients": "Burrata foam, truffle",
+        "price": 18.00
+    },
+    {
+        "category": "Starters",
+        "name": "Gazpacho",
+        "ingredients": "",
+        "price": 12.00
+    },
+    {
+        "category": "Starters",
+        "name": "Grilled artichoke",
+        "ingredients": "",
+        "price": 18.00
+    },
+    {
+        "category": "Starters",
+        "name": "Fried edamame",
+        "ingredients": "Red wine salt",
+        "price": 8.50
+    },
+    {
+        "category": "Starters",
+        "name": "Fried sweet potato",
+        "ingredients": "Truffle mayonnaise",
+        "price": 8.50
+    },
+    {
+        "category": "Main Courses",
+        "name": "Grilled Sea Bass",
+        "ingredients": "marinated mini-vegetables, Tom Yum broth",
+        "price": 36.00
+    },
+    {
+        "category": "Main Courses",
+        "name": "Red tuna",
+        "ingredients": "Edamame, wasabi mayonaise",
+        "price": 36.00
+    },
+    {
+        "category": "Main Courses",
+        "name": "Cod Kokotxas 'pil-pil'",
+        "ingredients": "Belly pork, seasonal mushrooms",
+        "price": 29.00
+    },
+    {
+        "category": "Main Courses",
+        "name": "Grilled Squid",
+        "ingredients": "Black rice, saffron 'alioli'",
+        "price": 32.00
+    },
+    {
+        "category": "Main Courses",
+        "name": "Catch of the day",
+        "ingredients": "",
+        "price": "Market price"
+    },
+    {
+        "category": "Main Courses",
+        "name": "Panang curry",
+        "ingredients": "Red rice, vegetables, naan bread",
+        "price": 19.00
+    },
+    {
+        "category": "Main Courses",
+        "name": "Homemade ravioli",
+        "ingredients": "Ricotta, spinach, walnuts",
+        "price": 26.00
+    },
+    {
+        "category": "Main Courses",
+        "name": "Beef tenderloin steak",
+        "ingredients": "Potato mille-feuille, port reduction",
+        "price": 34.00
+    },
+    {
+        "category": "Main Courses",
+        "name": "Suckling Pig",
+        "ingredients": "Plum, tomato",
+        "price": 31.00
+    },
+    {
+        "category": "Main Courses",
+        "name": "'Angus' Sirloin Steak Tataki'",
+        "ingredients": "Tempura 'piquillo peppers'",
+        "price": 35.00
+    },
+    {
+        "category": "Side Orders",
+        "name": "Truffled wedge potatoes with rocket",
+        "ingredients": "",
+        "price": 6.50
+    },
+    {
+        "category": "Side Orders",
+        "name": "Ceasar salad or green",
+        "ingredients": "",
+        "price": 6.50
+    },
+    {
+        "category": "Side Orders",
+        "name": "Japanese mushrooms sautéed",
+        "ingredients": "",
+        "price": 6.50
+    },
+    {
+        "category": "Side Orders",
+        "name": "Sautéed seasonal vegetables",
+        "ingredients": "",
+        "price": 6.50
+    },
+    {
+        "category": "Side Orders",
+        "name": "Padrón peppers",
+        "ingredients": "",
+        "price": 6.50
+    },
+    {
+        "category": "Dessert",
+        "name": "Homemade chocolate truffle",
+        "ingredients": "",
+        "price": 3.50
+    },
+    {
+        "category": "Dessert",
+        "name": "Mallorcan cheesecake",
+        "ingredients": "Toasted butter ice cream",
+        "price": 11.00
+    },
+    {
+        "category": "Dessert",
+        "name": "Chocolate coulant",
+        "ingredients": "Hazelnut & tonka bean ice cream",
+        "price": 10.00
+    },
+    {
+        "category": "Dessert",
+        "name": "Tarte tatin (for 2)",
+        "ingredients": "",
+        "price": 15.00
+    },
+    {
+        "category": "Dessert",
+        "name": "French toast 'coca de patata'",
+        "ingredients": "Sheep's milk ice cream",
+        "price": 10.00
+    },
+    {
+        "category": "Dessert",
+        "name": "Passion Fruit Pavlova",
+        "ingredients": "",
+        "price": 9.00
+    },
+    {
+        "category": "Dessert",
+        "name": "Spanish and local cheese selection",
+        "ingredients": "",
+        "price": 19.00
+    },
+    {
+        "category": "Dessert",
+        "name": "Home made ice cream",
+        "ingredients": "Sorbet",
+        "price": 4.50
+    },
+    {
+        "category": "Sweet Wine by the Glass",
+        "name": "MORTITX",
+        "ingredients": "Dolc de gel. Moscatel, Riesling",
+        "price": 8.00
+    },
+    {
+        "category": "Sweet Wine by the Glass",
+        "name": "APASIANADO DE JOSE PARIENTE",
+        "ingredients": "Sauvignon Blanc E",
+        "price": 7.00
+    },
+    {
+        "category": "Sweet Wine by the Glass",
+        "name": "VALVERAN 20",
+        "ingredients": "Manzanas Sidra de Hielo Raxao y Durona de Tresali",
+        "price": 7.00
+    },
+    {
+        "category": "Sweet Wine by the Glass",
+        "name": "OREMUS ASZU 3",
+        "ingredients": "Puttonyos Furmint, Harslevelu, Mascat de Lunel",
+        "price": 18.00
+    },
+    {
+        "category": "Kids",
+        "name": "Hamburger",
+        "ingredients": "French fries",
+        "price": 15.00
+    },
+    {
+        "category": "Kids",
+        "name": "Grilled beef",
+        "ingredients": "French fries or salad",
+        "price": 15.00
+    },
+    {
+        "category": "Kids",
+        "name": "Home Made Meatballs",
+        "ingredients": "Mashed potatoes",
+        "price": 15.00
+    },
+    {
+        "category": "Kids",
+        "name": "Grilled Fish",
+        "ingredients": "Rice or potatoes",
+        "price": 15.00
+    },
+    {
+        "category": "Kids",
+        "name": "Chicken Breast",
+        "ingredients": "French fries or salad",
+        "price": 15.00
+    },
+    {
+        "category": "Kids",
+        "name": "Pasta Bolognesa or Tomato Sauce",
+        "ingredients": "",
+        "price": 15.00
+    },
+    {
+        "category": "Kids Dessert",
+        "name": "Ice Cream",
+        "ingredients": "",
+        "price": 4.50
+    },
+    {
+        "category": "Kids Dessert",
+        "name": "Fruit Salad",
+        "ingredients": "",
+        "price": 4.00
+    },
+    {
+        "category": "Kids Dessert",
+        "name": "Milkshake",
+        "ingredients": "",
+        "price": 5.00
+    }
+]
