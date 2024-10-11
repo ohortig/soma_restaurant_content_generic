9667f5b40d28616a16d70abf6143ab4a82247554
HamzaB505 <hamzaboulaalaop@gmail.com>
2024-10-11 18:02:33 +0200

    fix: small changes in style

--- utils.py
+++ utils.py
@@ -3,18 +3,16 @@ from inputs.restaurant_info import RESTAURANT_INFO
 
 def item_exists(item_name):
     for item in RESTAURANT_INFO.dinner_menu:
-        if item_name.lower() in item["name"].lower() or item_name.lower() in item["ingredients"].lower():
+        if (item_name.lower() in item["name"].lower()) | (item_name.lower() in item["ingredients"].lower()):
             return True
     return False
 
 def nationality_exists(nationality):
     if nationality in RESTAURANT_INFO.nationalities:
         return True
-    else:
-        return False
+    return False
 
 def load_json(file_path):
-
     with open(file_path) as f:
         d = json.load(f)
         return (d)
